76ed6f9feb77c660c368bc50d3cd09b59a7ee89d
lilovejia <m15810479689@163.com>
2017-03-22 16:12:27 +0800

    update

--- src/main/java/java1702/javase/basic/chapter/EE1.java
+++ src/main/java/java1702/javase/basic/chapter/EE1.java
@@ -10,15 +10,31 @@ import java.util.Scanner;
  */
 //猜数字1.生成一个随机数，10000,100小了，1000小，100000大了。统计用户一共试了多少次
 public class EE1 {
+    private static int x;
+    private static int counter;
 
     public static void main(String[] args) {
-    Random random = new Random();
-    int x = random.nextInt();
-    System.out.println("");
-    Scanner scanner = new Scanner(System.in);
-    int y = scanner.nextInt();
-
-
+        Random random = new Random();
+        x = random.nextInt();
+        System.out.println(x);
+         guess();
     }
-
+    private static void guess() {
+        System.out.println("请输入一个数");
+        Scanner scanner = new Scanner(System.in);
+        int y = scanner.nextInt();
+        if (y > x) {
+            System.out.println("大了");
+            counter++;
+            guess();
+        } else if (y<x){
+            System.out.println("小了");
+            counter++;
+            guess();
+        } else{
+            System.out.println("猜对了");
+            counter++;
+            System.out.println(counter);
+        }
+    }
 }
