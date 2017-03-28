bb6ebfa10ff3d74dd1794064c302f1222a4d76b0
lilovejia <m15810479689@163.com>
2017-03-28 10:44:09 +0800

    update

---
+++ src/main/java/collection/StringTest.java
@@ -0,0 +1,11 @@
+package collection;
+
+/**
+ * Created by lixuanyu
+ * on 2017/3/28.
+ */
+public class StringTest {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/collection/_2DArray.java
+++ src/main/java/collection/_2DArray.java
@@ -1,10 +1,15 @@
 package collection;
 
+import java.awt.*;
+import java.util.Random;
+
 /**
  * Created by lixuanyu
  * on 2017/3/28.
  */
 public class _2DArray {
+    private static final  String[] COLORS = {"A","B","C","D","E","F"};
+
     public static void main(String[] args) {
         int[][] ints = new int[3][5];
         for (int i = 0; i < ints.length; i++) {
@@ -14,12 +19,12 @@ public class _2DArray {
             System.out.println();
         }
 
-       /* String[][] strings = new String[4][];
-        strings[0] = new String[5];
-        strings[1] = new String[2];
-        strings[2] = new String[3];
-        strings[3] = new String[4];不规则二维数组
-*/
+//        String[][] strings = new String[4][];
+//        strings[0] = new String[5];
+//        strings[1] = new String[2];
+//        strings[2] = new String[3];
+//        strings[3] = new String[4];
+
         double[][] doubles = {
                 {1d,2d,3d,4d,5d},
                 {1d}
@@ -29,6 +34,16 @@ public class _2DArray {
                 System.out.print(doubles[i][j] + "\t");
             }
             System.out.println();
+        }
+        String[][] strings = new String[10][10];
+
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = 0; j < strings[i].length; j++) {
+                strings[i][j] = COLORS[(int)(Math.random()*6)] ;//?why
+                System.out.print(strings[i][j] + "\t");
+            }
+            System.out.println();
+
         }
     }
 }
