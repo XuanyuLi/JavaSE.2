a8f6f93dfde7a8ef46c3f0f201fbd2401e474064
Administrator <2758697752@qq.com>
2017-03-29 10:56:48 +0800

    update

---
+++ src/main/java/collection/StringVsStringBufferTest.java
@@ -0,0 +1,23 @@
+package collection;
+
+/**
+ * Created by lixuanyu
+ * on 2017/3/29.
+ */
+public class StringVsStringBufferTest {
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public static void main(String[] args) {
+        String s = "";
+        StringBuffer stringBuffer = new StringBuffer(s);
+        Long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+//            s+=ALPHABET;
+            stringBuffer.append(ALPHABET);
+        }
+        System.out.println("time:" + (System.currentTimeMillis() - start));
+//        System.out.println(s.length());
+        System.out.println(stringBuffer.length());
+    }
+
+}
