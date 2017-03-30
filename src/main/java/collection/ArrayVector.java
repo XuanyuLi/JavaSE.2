81cfb3851e060f77323cfd27a572fbae45ec106b
Administrator <2758697752@qq.com>
2017-03-30 14:08:34 +0800

    update

---
+++ src/main/java/collection/ArrayVector.java
@@ -0,0 +1,14 @@
+package collection;
+
+import java.util.Vector;
+
+/**
+ * Created by lixuanyu
+ * on 2017/3/30.
+ */
+public class ArrayVector {
+    public static void main(String[] args) {
+        Vector<Integer > integers = new Vector<>();
+
+    }
+}
