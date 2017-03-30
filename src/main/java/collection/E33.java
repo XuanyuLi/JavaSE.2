7b97621185141d65d65e92b5856fd99fa544f7ff
Administrator <2758697752@qq.com>
2017-03-30 11:08:57 +0800

    update

--- src/main/java/collection/E33.java
+++ src/main/java/collection/E33.java
@@ -1,5 +1,7 @@
 package collection;
 
+import java.util.Arrays;
+
 /**
  * Created by lixuanyu
  * on 2017/3/29.
@@ -7,6 +9,7 @@ package collection;
 
  /*打印出杨辉三角形（要求打印出 n 行如下图）
              1
+            1 1
            1 2 1
           1 3 3 1
          1 4 6 4 1
@@ -14,5 +17,19 @@ package collection;
          数组
          */
 public class E33 {
-
+    public static void main(String[] args) {
+        int[] array1 = {1};
+        int[] array2 = {1, 1};
+        int[] array3 = {1, 2, 1};
+        int[] array4 = {1, 3, 3, 1};
+        int[] array5 = new int[5];
+        for (int i = 0; i < array5.length; i++) {
+            if (i == 0 || i == array5.length - 1) {
+                array5[i] = 1;
+            } else {
+                array5[i] = array4[i - 1] + array4[i];
+            }
+        }
+        System.out.println(Arrays.toString(array5));
+    }
 }
--- src/main/java/collection/HashtableTest.java
+++ src/main/java/collection/HashtableTest.java
@@ -1,6 +1,7 @@
 package collection;
 
 import java.util.Hashtable;
+import java.util.Map;
 
 /**
  * Created by lixuanyu
@@ -17,8 +18,16 @@ public class HashtableTest {
         System.out.println(hashtable);
         System.out.println(hashtable.get(2));
         for (Integer integer : hashtable.keySet()) {
-            System.out.println(integer + "->" + hashtable.get(integer));//哈希表的输出是无序的
-
+            System.out.println(integer + "->" + hashtable.get(integer));//哈希表的输出是无序的,keySet 获取键
+            //values 获取值，entrySet 既能获取键又能获取值
+        }
+        System.out.println("--------------------");
+        for (String s : hashtable.values()) {
+            System.out.println(s);
+        }
+        System.out.println("---------------------");
+        for (Map.Entry<Integer, String> integerStringEntry : hashtable.entrySet()) {
+            System.out.println(integerStringEntry.getKey() + "->" + integerStringEntry.getValue());
         }
     }
 }
