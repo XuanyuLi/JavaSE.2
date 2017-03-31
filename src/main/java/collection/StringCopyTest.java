c87bc04b8b72d6e27123fb19f556157e7f5c540a
Administrator <2758697752@qq.com>
2017-03-31 10:03:44 +0800

    update

---
+++ src/main/java/collection/StringCopyTest.java
@@ -0,0 +1,18 @@
+package collection;
+import java.util.Arrays;
+
+/**
+ * Created by lixuanyu
+ * on 2017/3/31.
+ */
+public class StringCopyTest {
+    public static void main(String[] args) {
+        int[] srcArray = {1,2,3,4,5,6};//source 源
+        int[] destArray = new int[10];//destination 目的地
+        System.arraycopy(srcArray,2,destArray,0,3);
+        System.out.println(Arrays.toString(destArray));
+        String[] strings = {"a","b","c","d","e","f","g","h","i","j"};
+        System.arraycopy(strings,3,strings,2,7);
+        System.out.println(Arrays.toString(strings));
+    }
+}
