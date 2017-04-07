1ddca9b0568bf649ffe76f760ff55df77e415259
Administrator <2758697752@qq.com>
2017-04-07 10:27:27 +0800

    update

--- src/main/java/java1702/javase/basic/newoop/Main.java
+++ src/main/java/java1702/javase/basic/newoop/Main.java
@@ -4,7 +4,7 @@ package java1702.javase.basic.newoop;
  * Created by lixuanyu
  * on 2017/4/7.
  */
-//创建了三个对象，两个地址，changeReference方法并没有改变对象f的指向，只是把a对象的指向由原来的值"f"改变为值"b"
+//创建了四个对象，三个地址，changeReference方法并没有改变对象f的指向，只是把a对象的指向由原来的值"f"改变为值"b"
 //本质上还是值传递（传递的是对象的地址的值→特殊的值）
 public class Main {
     public static void main(String[] args) {
