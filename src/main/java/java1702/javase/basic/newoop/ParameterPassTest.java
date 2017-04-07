8f90fa9d9f6812d65d2c3701045ad100943506e5
Administrator <2758697752@qq.com>
2017-04-07 09:06:59 +0800

    update

--- src/main/java/java1702/javase/basic/newoop/ParameterPassTest.java
+++ src/main/java/java1702/javase/basic/newoop/ParameterPassTest.java
@@ -11,10 +11,33 @@ public class ParameterPassTest {
         System.out.println("c:" + i);
     }
 
+    private static void stringTest(String string) {
+        System.out.println("b:" + string);
+        string = "goodbye";
+        System.out.println("c:" + string);
+    }
+
+    private static void integerTest(Integer integer1) {
+        System.out.println("b:" + integer1);
+        integer1 = 888;
+        System.out.println("c:" + integer1);
+    }
+
     public static void main(String[] args) {
+        String string = "good";
         int i = 0;
+        Integer integer = 666;
         System.out.println("a:" + i);
         test(i);//实际参数
         System.out.println("d:" + i);//输出结果为0，方法的参数属于基本数据类型，方法中的改变不影响实际参数
+        System.out.println("---------------------------");
+        System.out.println("a:" + string);
+        stringTest(string);
+        System.out.println("d:" + string);
+        System.out.println("----------------------------");
+        System.out.println("a:" + integer);
+        integerTest(integer);
+        System.out.println("d:" + integer);
+
     }
 }
