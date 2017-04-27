8ed260b189d0864e7c45ac617a25d89b45f64b5a
Administrator <2758697752@qq.com>
2017-04-27 11:15:58 +0800

    update

--- src/main/java/exam3/E4.java
+++ src/main/java/exam3/E4.java
@@ -13,22 +13,25 @@ import java.util.Random;
 (2)从文件中读取这5000个整数,并计算其最大值、最小值和平均值并输出结果。*/
 public class E4 {
     public static void main(String[] args) {
-        try {
-            RandomAccessFile randomAccessFile = new RandomAccessFile("txt","rw");
-            Random random = new Random();
-            byte[] bytes = new byte[5000];
-            for (int i = 0; i < 5000; i++) {
-                randomAccessFile.writeInt(random.nextInt(9999));
+//        try {
+//            RandomAccessFile randomAccessFile = new RandomAccessFile("txt","rw");
+//            Random random = new Random();
+//            byte[] bytes = new byte[5000];
+//            for (int i = 0; i < 5000; i++) {
+//                randomAccessFile.writeInt(random.nextInt(9999));
 //                randomAccessFile.read(bytes,0,5000);
-
-            }
-            Reader reader = new FileReader("txt");
-            int i;
-            if ((i = reader.read()) != -1) {
-                System.out.println((char)i);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+//
+//            }
+//            Reader reader = new FileReader("txt");
+//            int i;
+//            if ((i = reader.read()) != -1) {
+//                System.out.println((char)i);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        for (int i = 0; i < 5000; i++) {
+            int r = (int)(Math.random()*9999+1);
         }
     }
 }
--- src/main/java/exam3/E5.java
+++ src/main/java/exam3/E5.java
@@ -11,26 +11,26 @@ import java.util.List;
 /*编写一个程序,将Fibonacii数列的前20项写入一个随机访问文件raf.txt
 然后从该文件中读出第2、4、6等偶数位置上的项并将它们依次写入另一个文件output.txt*/
 public class E5 {
+    private static int fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
     public static void main(String[] args) {
-        long[] longs = new long[20];
-        for (int i = 0; i < 20; i++) {
-            if (i == 0 | i ==1) {
-                longs[i] = 1;
-            } else{
-                longs[i] = longs[i-1] + longs[i-2];
-            }
-        }
-        for (long aLong : longs) {
-            System.out.println(aLong);
-        }
         try {
             RandomAccessFile randomAccessFile = new RandomAccessFile("raf.txt","rw");//?
-            Reader reader = new FileReader("raf.txt");
-            Writer writer = new FileWriter("output.txt");
-            int i;
-            if ((i = reader.read()) != -1) {
-                writer.write(i);
-                writer.flush();
+            for (int i = 0; i < 20; i++) {
+                randomAccessFile.writeInt(fibonacci(i+1));
+            }
+            randomAccessFile.seek(0);
+            for (int i = 0; i < 10; i++) {
+                randomAccessFile.readInt();
+                try (RandomAccessFile file = new RandomAccessFile("output.txt", "rw")) {
+                    int r = randomAccessFile.readInt();
+                    System.out.println(r);
+                    file.writeInt(r);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
