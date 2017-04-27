7b1d0bff90ed72498515b00e88b57f468b150ec3
Administrator <2758697752@qq.com>
2017-04-27 09:28:27 +0800

    update

---
+++ src/main/java/regex/Test.java
@@ -0,0 +1,21 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by lixuanyu
+ * on 2017/4/27.
+ */
+public class Test {
+    public static void main(String[] args) {
+//        Pattern 模式  Matcher 匹配
+        Pattern pattern = Pattern.compile("l");//JS var pattern = /e/;
+        Matcher matcher = pattern.matcher("Hello");
+//        System.out.println(matcher.find());
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+    }
+}
