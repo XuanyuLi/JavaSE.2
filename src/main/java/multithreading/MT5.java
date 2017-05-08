49fccfd55c8de6f15ec8da08133a8e0445f4e880
Administrator <2758697752@qq.com>
2017-05-08 17:03:09 +0800

    update

---
+++ src/main/java/multithreading/MT5.java
@@ -0,0 +1,37 @@
+package multithreading;
+
+/**
+ * Created by lixuanyu
+ * on 2017/5/8.
+ */
+public class MT5 implements Runnable {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MT5());
+        thread1.setName("thread 1");
+        Thread thread2 = new Thread(new MT5());
+        thread2.setName("thread 2");
+        Thread thread3 = new Thread(new MT5());
+        thread3.setName("thread 3");
+        thread1.start();
+        thread2.start();
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread3.start();
+        System.out.println("test...");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(Thread.currentThread().getName() + " is running...");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
