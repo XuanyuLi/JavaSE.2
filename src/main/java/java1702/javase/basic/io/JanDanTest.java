9b6a141ffe181518659c0b7edfe6eb89e028145f
Administrator <2758697752@qq.com>
2017-05-08 16:03:05 +0800

    update

--- src/main/java/java1702/javase/basic/io/JanDanTest.java
+++ src/main/java/java1702/javase/basic/io/JanDanTest.java
@@ -1,28 +1,46 @@
 package java1702.javase.basic.io;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.ConnectException;
 import java.net.MalformedURLException;
 import java.net.URL;
 /**
  * Created by lixuanyu
  * on 2017/4/21.
  */
-public class JanDanTest {
+public class JanDanTest implements Runnable{
+    private static final String PAGE_URL = "http://jandan.net/tag/%E5%A4%A9%E6%96%87/page/";
+    private int page;
+
+    public JanDanTest(int page) {
+        this.page = page;
+    }
+
     public static void main(String[] args) {
+        for (int i = 0; i < 18; i++) {
+            Thread thread = new Thread(new JanDanTest(i + 1));
+            thread.start();
+            System.out.println("page: " + (i + 1));
+        }
+    }
+
+    @Override
+    public void run() {
+        int counter = 0;
         try {
-            URL url = new URL("http://jandan.net/tag/%E5%A4%A9%E6%96%87");
+            URL url = new URL(PAGE_URL + page);
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    if (line.contains("data-original=\"//")) {
-                        String imageURL = "http:" + line.substring(line.indexOf("//tankr"), line.indexOf("jpg") + 3);
-                        System.out.println(imageURL);
+                    if (line.contains("data-original")) {
+                        String imageUrl = line.substring(line.indexOf("data-original=\"") + "data-original=\"".length(), line.indexOf("\" width"));
+                        if (!imageUrl.startsWith("http")) {
+                            imageUrl = "http:" + imageUrl;
+                        }
+                        String extension = imageUrl.substring(imageUrl.lastIndexOf("."));
+                        download(imageUrl, extension, page, ++counter);
                     }
                 }
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -31,9 +49,25 @@ public class JanDanTest {
         }
     }
 
-    private static void download(String imageUrl) {
+    private static void download(String imageUrl, String extension, int page, int counter) {
         try {
             URL url = new URL(imageUrl);
+            try (
+                    BufferedInputStream in = new BufferedInputStream(url.openStream());
+                    BufferedWriter out = new BufferedWriter(new FileWriter("images/" + page + "-" + counter + extension))
+            ) {
+                int i;
+                while ((i = in.read()) != -1) {
+                    out.write(i);
+                }
+                System.out.println("\t" + page + "-" + counter);
+            } catch (ConnectException e) {
+                System.out.println("timeout...");
+//                download(imageUrl, extension);
+                System.out.println(imageUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
--- src/main/java/multithreading/Test.java
+++ src/main/java/multithreading/Test.java
@@ -8,14 +8,14 @@ public class Test implements Runnable{
     public static void main(String[] args) {
         //进程 process   线程 thread
         Test test = new Test();
-        Thread thread = new Thread(test);
-        thread.start();
+        Thread thread = new Thread(test);//New
+        thread.start();//Runnable
 
         System.out.println("test...");
     }
 
     @Override
-    public void run() {
+    public void run() {//Running
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
@@ -27,3 +27,4 @@ public class Test implements Runnable{
 //        }
 //    }
 }
+//时间片 CPU time slice  CPU 在一个时间点上只能做一件事情
