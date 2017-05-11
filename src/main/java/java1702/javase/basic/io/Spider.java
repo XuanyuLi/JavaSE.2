fcdb47d81e5a02760641758d73a1048d3a58f0f1
Administrator <2758697752@qq.com>
2017-05-11 14:20:25 +0800

    update

--- src/main/java/java1702/javase/basic/io/Spider.java
+++ src/main/java/java1702/javase/basic/io/Spider.java
@@ -1,5 +1,4 @@
 package java1702.javase.basic.io;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -15,7 +14,7 @@ public class Spider {
     public static void main(String[] args) throws IOException {
         String url = "http://bj.lianjia.com/ershoufang/dongcheng";
         Document document = Jsoup.connect(url).get();
-        Elements elements = document.select("li[class=clear");
+        Elements elements = document.select("li[class=clear]");
         for (Element element : elements) {
             String region = element.select("a[data-el=region]").get(0).text();
             System.out.println(region);
